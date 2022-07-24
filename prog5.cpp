aed7b779fc8b3773319c19fbe5c1da4f63a6f2d7
Aneesh M Bhat <aneeshbhat3719@gmail.com>
2022-07-24 12:25:37 +0530

    done

--- prog5.cpp
+++ prog5.cpp
@@ -120,20 +120,19 @@ void display()
     glFlush();
 }
 
-void myinit()
+void init()
 {
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Cohen Sutherland Line Clipping Algorithm");
     glClearColor(0, 0, 0, 1);
     gluOrtho2D(0, 500, 0, 500);
+    glutDisplayFunc(display);
+    glutMainLoop();
 }
 
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(500, 500);
-    // glutInitWindowPosition(0, 0);
-    glutCreateWindow("Cohen Sutherland Line Clipping Algorithm");
-    myinit();
-    glutDisplayFunc(display);
-    glutMainLoop();
+    init();
 }
